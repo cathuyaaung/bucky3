9c88a03072e33450cef3c61fc0d04ee5119bc6d6
cizrage <thu.ya.a@gmail.com>
2016-11-09 23:02:51 +0800

    69 - Drop down list

--- bucky/src/bucky/Gui2.java
+++ bucky/src/bucky/Gui2.java
@@ -1,5 +1,38 @@
 package bucky;
 
-public class Gui2 {
+import java.awt.Event;
+import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Gui2 extends JFrame {
+	private JComboBox box;
+	private JLabel picture;
+	
+	private static String[] filenames = {"check.png", "cross.png"};	
+	private Icon[] pics = {new ImageIcon(getClass().getResource(filenames[0])),new ImageIcon(getClass().getResource(filenames[1]))};
+	
+	public Gui2(){
+		super("title");
+		setLayout(new FlowLayout());
+		
+		box = new JComboBox(filenames);
+		box.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange()==ItemEvent.SELECTED) {
+					picture.setIcon(pics[box.getSelectedIndex()]);
+				}
+			}
+		});
+		
+		add(box);
+		picture = new JLabel(pics[0]);
+		add(picture);
+	}
 }
--- bucky/src/bucky/apple.java
+++ bucky/src/bucky/apple.java
@@ -5,7 +5,8 @@ import javax.swing.JFrame;
 public class apple {
 
 	public static void main(String args[]){
-		Gui g = new Gui();
+//		Gui g = new Gui();
+		Gui2 g = new Gui2();
 		g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		g.setSize(300,300);
 		g.setVisible(true);
